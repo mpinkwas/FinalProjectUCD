0642e9cc79e9d3a414b8ccfa10e077877a61e2ef
maciapinkwas <mpinkwas@gmail.com>
2021-06-18 16:43:34 +0100

    update 1.2

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 # This is my final project on the rising house prices in Ireland
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
+import pip as pip
 import matplotlib.pyplot as plt
+import pandas as pd
+data = pd.read_csv("Property_Price_Register_Ireland-05-04-2019.csv")
+print(data)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
