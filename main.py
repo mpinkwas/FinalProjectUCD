56701fd512d985865d73e3dcc58e31f5e77c210e
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 00:53:11 +0100

    update 1.6 added both data sets

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pip as pip
 data = pd.read_csv('./content/Property_Price_Register_Ireland-05-04-2019.csv')
-print(data)
+data_all = pd.read_csv('./content/PPR-ALL.csv')
+print(data_all)
+
+
 
 
 
