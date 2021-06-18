f7876ab3d2286f7d969a21fc40e8e53f8771e1a9
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 00:09:24 +0100

    update 1.5

--- main.py
+++ main.py
@@ -2,4 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pip as pip
+data = pd.read_csv('./content/Property_Price_Register_Ireland-05-04-2019.csv')
+print(data)
+
+
+
 
