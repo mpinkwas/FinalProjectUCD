b16f0971b6421c2b2dbb8838d003b19ce1700554
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 19:07:07 +0100

    update 2.0 amended pd.read.csv

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ import requests
 from pandas import read_csv
 import numpy as np
 
-data_2017 = pd.read_csv('./content/PPR-2017-Dublin.csv')
-data_2018 = pd.read_csv('./content/PPR-2018-Dublin.csv')
-data_2019 = pd.read_csv('./content/PPR-2019-Dublin.csv')
-print(type(data_2017))
-print(type(data_2018))
-print(type(data_2019))
-print(data_2019, data_2018, data_2017)
-print(data_2019.iloc[:5])
+data_2017 = pd.read_csv('./content/PPR-2017-Dublin.csv'
+                        , header=0
+                        , usecols=["Date of Sale (dd/mm/yyyy)", "Postal Code", "Price (�)"])
+data_2018 = pd.read_csv('./content/PPR-2018-Dublin.csv'
+                        , header=0
+                        , usecols=["Date of Sale (dd/mm/yyyy)", "Postal Code", "Price (�)"])
+data_2019 = pd.read_csv('./content/PPR-2019-Dublin.csv'
+                        , header=0
+                        , usecols=["Date of Sale (dd/mm/yyyy)", "Postal Code", "Price (�)"])
+
+#print(type(data_2017))
+#print(type(data_2018))
+#print(type(data_2019))
+#print(data_2019, data_2018, data_2017)
+print(data_2019)
+print(data_2018)
+print(data_2017)
+
+
+
+
 
 
 
