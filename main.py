55e70167f785f548d8f9ac06d01dba2a91772726
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 13:23:08 +0100

    update 1.7 removed 2019 ds

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # This is my final project on the rising house prices in Ireland
+from urllib import request
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pip as pip
+import requests
 data_all = pd.read_csv('./content/PPR-ALL.csv')
-print(data_all)
+print(data_all.info)
+data = requests.get("https://priceregister.civictech.ie/api/sales")
+
+
+
+
+
+
+
+
 
 
 
