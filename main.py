011f9b88126d3323575d8c0662f446344e5b3adf
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 01:09:26 +0100

    update 1.7 removed 2019 ds

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pip as pip
-data = pd.read_csv('./content/Property_Price_Register_Ireland-05-04-2019.csv')
 data_all = pd.read_csv('./content/PPR-ALL.csv')
 print(data_all)
+
 
 
 
