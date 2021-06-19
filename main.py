49a689044a67909aea767535add2909057433cf1
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 14:40:35 +0100

    update 1.8 added DS from 2017-2019

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pip as pip
 import requests
-data_all = pd.read_csv('./content/PPR-ALL.csv')
-print(data_all.info)
+data_2017 = pd.read_csv('./content/PPR-2017-Dublin.csv')
+data_2018 = pd.read_csv('./content/PPR-2018-Dublin.csv')
+data_2019 = pd.read_csv('./content/PPR-2019-Dublin.csv')
+data_all = data_2019, data_2018, data_2017
+print(data_all)
 data = requests.get("https://priceregister.civictech.ie/api/sales")
 
 
