0e68957f3a9bf0b7eb65162cd958bddb6563dcc0
maciapinkwas <mpinkwas@gmail.com>
2021-06-19 18:40:58 +0100

    update 1.9 removed "data_all"

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-# This is my final project on the rising house prices in Ireland
-from urllib import request
-
-import numpy as np
+# This is my final project on house prices in Ireland from the years 2017, 2018 and 2019
 import matplotlib.pyplot as plt
 import pandas as pd
-import pip as pip
 import requests
+from pandas import read_csv
+import numpy as np
+
 data_2017 = pd.read_csv('./content/PPR-2017-Dublin.csv')
 data_2018 = pd.read_csv('./content/PPR-2018-Dublin.csv')
 data_2019 = pd.read_csv('./content/PPR-2019-Dublin.csv')
-data_all = data_2019, data_2018, data_2017
-print(data_all)
-data = requests.get("https://priceregister.civictech.ie/api/sales")
+print(type(data_2017))
+print(type(data_2018))
+print(type(data_2019))
+print(data_2019, data_2018, data_2017)
+print(data_2019.iloc[:5])
+
+
+
+
+
+
+
+
+
 
 
 
