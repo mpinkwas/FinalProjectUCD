7dcd2faac2a72e72af9895d68ce78445bacc9d70
maciapinkwas <mpinkwas@gmail.com>
2021-06-21 08:56:53 +0100

    update 2.3 CHANGED PROJECT

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is my final project on house prices in Ireland from the years 2017, and 2019
+# This is my final project on the number of vaccinations per country by vaccine manufacturers.
 import sql_connectors as con
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,29 +8,9 @@ import numpy as np
 from sqlalchemy import create_engine
 import seaborn as sns
 
-data_2017 = pd.read_csv('./content/PPR-2017-Dublin1.csv'
-                        , header=0
-                        , usecols=["Date of Sale (dd/mm/yyyy)", "Postal Code", "Price Euro"])
-data_2019 = pd.read_csv('./content/PPR-2019-Dublin1.csv'
-                        , header=0
-                        , usecols=["Date of Sale (dd/mm/yyyy)", "Postal Code", "Price Euro"])
+df = pd.read_csv('./content/country_vaccinations_by_manufacturer.csv')
 
-#print(type(data_2017))
-#print(type(data_2019))
-#print(data_2019, data_2017)
-print(data_2019)
-print(data_2017)
-data_2017.replace(to_replace = np.nan, value = 'Dublin 36')
-print(data_2017)
-
-import sqlite3 as sqlite
-import sql as sql
-import sqlalchemy as sqla
-import _sqlite3 as sqlite3
-engine_1 = create_engine('sqlite:///PPR-2017-Dublin.csv')
-engine_2 = create_engine('sqlite:///PPR-2019-Dublin.csv')
-con_1 = engine_1.connect
-con_2 = engine_2.connect
+print(df)
 
 
 
