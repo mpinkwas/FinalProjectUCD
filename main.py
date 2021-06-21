50e938833b3e2ffc81a05ae2bd2efa2f286d69e5
maciapinkwas <mpinkwas@gmail.com>
2021-06-21 09:24:11 +0100

    update 2.3 np.update

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from pandas import read_csv
 import numpy as np
 from sqlalchemy import create_engine
 import seaborn as sns
+import csv
 
 df = pd.read_csv('./content/country_vaccinations_by_manufacturer.csv')
-
 print(df)
-
+mean = np.mean(df['total_vaccinations'])
+print(mean)
+median = np.median(df['total_vaccinations'])
+print(median)
 
 
 
