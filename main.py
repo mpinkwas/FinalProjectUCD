eca38fb7321297b577c0ef0713b06416e57268f8
maciapinkwas <mpinkwas@gmail.com>
2021-06-21 10:07:09 +0100

    update 2.4

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ import numpy as np
 from sqlalchemy import create_engine
 import seaborn as sns
 import csv
-
+#
 df = pd.read_csv('./content/country_vaccinations_by_manufacturer.csv')
 print(df)
+#Finding mean and median of total vaccinations in whole data set
 mean = np.mean(df['total_vaccinations'])
 print(mean)
 median = np.median(df['total_vaccinations'])
 print(median)
+
+#Checking for NaN in df -- none found
+check_for_nan = df.isnull().values.any()
+print (check_for_nan)
+
+#Checking for duplicates -- none found
+check_for_dup = df.duplicated().values.any()
+print(check_for_dup)
 
 
 
